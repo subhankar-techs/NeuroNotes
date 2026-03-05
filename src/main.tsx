a7da6860c0f7af3ba8afabbd3b7c8f4157e24b8b
SUBHANKAR <mannasubhankar06@gmail.com>
2026-03-05 12:04:41 +0530

    Some changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = import.meta.env.PROD ? 'https://neuronotes-backend.onrender.com/' : 'http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
